594afad020a68f9e12f1639acfcdb9f2ceccebe0
fdamasco <fe.damasco@hotmail.com>
2022-10-14 19:52:48 -0300

    initial commit

---
+++ .storybook/preview.cjs
@@ -0,0 +1,18 @@
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
+
+import '../src/styles/global.css';
+
+import { themes } from '@storybook/theming'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme: collapseTextChangeRangesAcrossMultipleVersions.dark,
+  },
+}
---
+++ src/components/Heading.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Heading, HeadingProps } from "./heading";
+
+export default {
+  title: "Components/Heading",
+  component: Heading,
+  args: {
+    children: "Lorem ipsum.",
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    }
+  }
+} as Meta<HeadingProps>;
+
+export const Default: StoryObj = {};
+
